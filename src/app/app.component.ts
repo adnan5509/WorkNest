54d46b34109ef50fc82291ce774a12f1fcd90600
adnan5509 <adnanafzal5509@gmail.com>
2025-04-02 15:59:43 +0200

    refactor: create dashboard-item component for better structure and reusability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./components/header/header.component";
 import { ServerStatusComponent } from "./components/dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./components/dashboard/traffic/traffic.component";
 import { SupportTicketsComponent } from "./components/dashboard/support-tickets/support-tickets.component";
+import { DashboardItemComponent } from "./components/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
 
