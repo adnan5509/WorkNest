b1610d0c962938d1f69d1c0440b2101113b11460
adnan5509 <adnanafzal5509@gmail.com>
2025-04-04 13:46:52 +0200

    refactor: update control component to use HostBinding for class management

--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,9 +7,13 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: { class: 'control' },
 })
 export class ControlComponent {
 
+  @HostBinding('class') className = 'control';
+
   @Input({ required: true }) title!: string;
+
+
+
 }
